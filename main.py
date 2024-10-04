122f1a42fa18370fccd20ce2604c57ce25309fdc
Martin Røvang <martinrovang@gmail.com>
2024-10-04 11:22:58 +0200

    small clean

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import create_cone
 import theoretical
 import neomedsys
 import pyrad
+from rich import print
+from rich.console import Console
+from rich.table import Table
 
 # Parameters for the cone
 radius = 20
@@ -24,6 +27,26 @@ neomedsys_surface_area = neomedsys.calculate_surface_area_skimage_mm2(cone, reso
 
 pyrad_surface_area = pyrad.calculate_surface_area_pyrad(cone, resolution)
 
-print(f"Theoretical surface area: {theoretical_surface_area} mm²")
-print(f"NeoMedSys surface area: {neomedsys_surface_area} mm²")
-print(f"Pyrad surface area: {pyrad_surface_area} mm²")
+# the differences in %
+diff_neomedsys = (theoretical_surface_area - neomedsys_surface_area) / theoretical_surface_area * 100
+diff_pyrad = (theoretical_surface_area - pyrad_surface_area) / theoretical_surface_area * 100
+
+
+# Create a console instance
+console = Console()
+
+# Create a table
+table = Table(title="Surface Area Comparison")
+
+# Add columns
+table.add_column("Description", justify="left", style="cyan", no_wrap=True)
+table.add_column("Surface Area (mm²)", justify="right", style="magenta")
+table.add_column("Error (%)", justify="right", style="yellow")
+
+# Add rows with data
+table.add_row("Theoretical Surface Area", f"{theoretical_surface_area:.2f}", "")
+table.add_row("NeoMedSys Surface Area", f"{neomedsys_surface_area:.2f}", f"{diff_neomedsys:.2f}")
+table.add_row("Pyrad Surface Area", f"{pyrad_surface_area:.2f}", f"{diff_pyrad:.2f}")
+
+# Print the table
+console.print(table)
